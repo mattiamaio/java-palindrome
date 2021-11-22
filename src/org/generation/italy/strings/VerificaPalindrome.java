89d06c05ab440bc888e0a38a0e6f1cd0918a7d4a
Mattia Maio <mattia.maio10@gmail.com>
2021-11-22 15:01:09 +0100

    mini correzione

--- src/org/generation/italy/strings/VerificaPalindrome.java
+++ src/org/generation/italy/strings/VerificaPalindrome.java
@@ -6,28 +6,29 @@ public class VerificaPalindrome {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		String input = "";
 		String inputReverse = "";
-		
+
 		System.out.print("Scrivi qui una parola: ");
 		input = scan.nextLine();
-		
-		for(int i = input.length()-1; i >= 0; i--) {
+
+		for (int i = input.length() - 1; i >= 0; i--) {
 			inputReverse += input.charAt(i);
-		}	
-		
-		if(inputReverse.equals(input)) {
-			System.out.println("Il contrario della parola inserita è " + inputReverse + " ed è lunga " + inputReverse.length() + " caratteri");
+		}
+
+		if (inputReverse.equals(input)) {
+			System.out.println("Il contrario della parola inserita è " + inputReverse + " ed è lunga "
+					+ inputReverse.length() + " caratteri");
 			System.out.println("La parola inserita è un palindromo");
 		} else {
 			System.out.println("Il contrario della parola inserita è: " + inputReverse);
 			System.out.println("La parola inserita non è un palindromo");
-			
+
 		}
-			
+
 		scan.close();
-		
+
 	}
 
 }
